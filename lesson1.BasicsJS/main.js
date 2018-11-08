f80a0ab79a7142ae76b2307c488945c58e9143be
DmitriyTitarenko <dmutro.tutarenko@gmail.com>
2018-11-08 14:51:08 +0200

    end lesson1

---
+++ lesson1.BasicsJS/main.js
@@ -0,0 +1,107 @@
+// WORK WITH VARIABLES
+
+
+
+// var num = 3;
+// alert(num);
+
+// done
+
+// var a = 10;
+// var b = 2;
+// alert(a - b);
+// alert(a + b);
+// alert(a * b);
+// alert(a / b);
+
+// done 
+
+// var c = 15;
+// var d = 2;
+// var result = c + d;
+// alert(result);
+
+// done
+
+// var a = 10, b = 2, c = 5;
+// alert(a + b + c);
+
+// done
+
+// var a = 17, b = 10;
+// var c = a - b;
+// var d = 7;
+// var result = c + d;
+// alert(result);
+
+// done
+
+
+// END WORK WITH VARIABLES
+
+// WORK WITH STRINGS
+
+// var str = "Hi, world";
+// alert(str);
+
+// done
+
+// var str1 ="Hi,";
+// var str2 ="World";
+// alert(str1 + str2);
+
+// var myName = "Dmitriy";
+// alert("Hi, " + myName);
+
+// var age = 23;
+// alert("Your age" + age + "years old");
+
+// var name = prompt("write name");
+// alert("Your name is" + name);
+
+// var num = prompt('write num');
+// alert(num * num); 
+
+// var str = 'abcde';
+// alert(str[0]);
+// alert(str[1]);
+// alert(str[2]);
+// alert(str[3]);
+// alert(str[4]);
+
+// var num = 12345;
+// var result = num[1] * num[2] * num[3] * num[4];
+// alert(result);
+
+// var inSec = 60;
+// var inHour = inSec * 60;
+// var inDay = inHour * 24;
+// var inMounth = inDay *30;
+
+// alert(inSec, inHourm, inDay, inMounth);
+
+
+// var hour = '16';
+// var min = '05';
+// var sec = '59';
+// alert(hour + ':' + min + ':' + sec);
+
+// var num = 3;
+// alert(num * num);
+
+// var num = 47;
+// num += 7;
+// num -= 18;
+// num *= 10;
+// num /= 15;
+// alert(num);
+
+// var num = 10;
+// num++;
+// num++;
+// num--;
+// alert(num);
+
+
+
+// FINALLY END LESSON1

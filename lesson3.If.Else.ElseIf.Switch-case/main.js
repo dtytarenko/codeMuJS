0c4e83b134c4e4989b4813a9de32f36fa63c72c3
DmitriyTitarenko <dmutro.tutarenko@gmail.com>
2018-11-09 15:05:04 +0200

    end lesson 3

---
+++ lesson3.If.Else.ElseIf.Switch-case/main.js
@@ -0,0 +1,170 @@
+// var a = 0;
+// if (a == 0) {
+//     alert('true');
+// }   else {
+//     alert('false');
+// }
+
+// var a = 0;
+// if (a > 0) {
+//     alert('true');
+// }   else {
+//     alert('false');
+// }
+
+// var a = 0;
+// if (a < 0) {
+//     alert('true');
+// }   else {
+//     alert('false');
+// }
+
+// var a = 0;
+// if (a >= 0) {
+//     alert('true');
+// }   else {
+//     alert('false');
+// }
+
+// var a = 0;
+// if (a <= 0) {
+//     alert('true');
+// }   else {
+//     alert('false');
+// }
+
+// var a = 0;
+// if (a != 0) {
+//     alert('true');
+// }   else {
+//     alert('false');
+// }
+
+// var a = 'test';
+//     if (a == 'test'){
+//         alert('true');
+//     } else {
+//         alert('false');
+//     }
+
+// var a = 1;
+// if (a === 1) {
+//     alert('true');
+// } else {
+//     alert('false');
+// }
+
+
+// var test = true;
+// if (test) {
+//     alert('true');
+// } else {
+//     alert('false');
+// }
+
+// var test = true;
+// if (!test) {
+//     alert('true');
+// } else {
+//     alert('false');
+// }
+
+// var a = 5;
+//     if (a > 0 && a < 5) {
+//         alert('true');
+//     } else {
+//         alert('false');
+//     }
+
+// var a = 5;
+//     if (a == 0 || a == 2){
+//         a += 7;
+//     } else {
+//         a /= 10;
+//     }
+// alert(a);
+
+
+// var a = 1;
+// var b = 2;
+//     if (a <= 1 && b >= 3) {
+//         alert(a + b);
+//     } else {
+//         alert(a - b);
+//     }
+
+// var a = 2;
+// var b = 4;
+
+// if ((a > 2 || a < 11) && (b >=6 || b < 14)) {
+//     alert('true');
+//     } else {
+//         alert('false');
+//     }
+
+// var num = 1;
+// switch (num) {
+// 	case 1: result = 'зима';
+// 	break;
+// 	case 2: result = 'весна';
+// 	break;
+// 	case 3: result = 'лето';
+// 	break;
+// 	case 4: result = 'осень';
+// 	break;
+// }
+// alert(result);
+
+// var day = 4;
+//     if (day >= 0 && day <=10) {
+//         alert('first period');
+//     }
+//     if (day >=11 && day <=20) {
+//         alert('second period');
+//     }
+//     if (day >=21 && day <=31); {
+//         alert('third period');
+//     }
+        
+
+// var month = 2;
+//     if (month == 12 && month >= 2) {
+//         var result = 'winter'; 
+//     }
+//     if (month >= 3 || month <= 5) {
+//         var result = 'spring';
+//     }
+//     if (month >=6 || month <=8) {
+//         var result = 'summer';
+//     }
+//     if (month >=9 || month <=11) {
+//         var result = 'autumn';
+//     }
+//     alert(result);
+
+// var a = 'adcde';
+//     if (a[0] == 'a' ){
+//         alert('yes');
+//     }   else {
+//         alert ('no');
+//     }
+
+// var str = '12345';
+// if (str[0] == '1' || str[0] == '2' || str[0] == '3') {
+// 	alert('Да');
+// } else {
+// 	alert('Нет');
+// }
+
+// var str = '123';
+// var sum = Number(str[0]) + Number(str[1]) + Number(str[2]);
+// alert(sum);
+
+// var str = '123456';
+// var str1 = Number(str[0]) + Number(str[1]) + Number(str[2]);
+// var str2 = Number(str[3]) + Number(str[4]) + Number(str[5]);
+// if (str1 == str2) {
+// 	alert('Да');
+// } else {
+// 	alert('Нет');
+// }

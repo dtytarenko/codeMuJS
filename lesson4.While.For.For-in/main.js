55ef50d16725a9cf51dd65803807e7bad87de87e
DmitriyTitarenko <dmutro.tutarenko@gmail.com>
2018-11-15 21:25:56 +0200

    learn for

---
+++ lesson4.While.For.For-in/main.js
@@ -0,0 +1,18 @@
+// for (var i = 1; i <=100; i++) {
+//     document.write(i + '<br>');
+// }
+
+// for (var i = 11; i <= 33; i++){
+//     document.write(i + '<br>');
+// }
+
+// for (var i = 0; i <=100; i = i + 2) {
+//     document.write(i + '<br>');
+// }
+
+// var result = 0;
+// for (var i = 0; i <= 100; i++){
+//     result += i;
+// }
+// alert(result);
+

e4edfaab4a43f0d28148758997a041aff4747251
DmitriyTitarenko <dmutro.tutarenko@gmail.com>
2018-11-19 18:48:14 +0200

    learn math function

---
+++ lesson5.Mathfunction/main.js
@@ -0,0 +1,17 @@
+// var a = 10, b = 3;
+// alert(a % b);
+
+
+// var a = 10, b = 3;
+// var result = a % b;
+// if (result != 0) {
+//     alert('delitsya s ostatkom' + result)
+// } else {
+//     alert('delitsya' + (a / b));
+// }
+
+// var st = Math.pow(2, 10);
+// alert(st);
+
+var b = Math.sqrt(245);
+alert(b);

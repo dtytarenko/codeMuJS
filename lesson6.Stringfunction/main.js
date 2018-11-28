ddf05c7788378844fe32b0acdd72005f6c99ac89
DmitriyTitarenko <dmutro.tutarenko@gmail.com>
2018-11-28 17:32:30 +0200

    end lesson 6.StringFunction

---
+++ lesson6.Stringfunction/main.js
@@ -0,0 +1,64 @@
+// var some = 'js';
+// alert(some.toUpperCase());
+
+// var some = 'JS';
+// alert(some.toLowerCase());
+
+// var some = 'я учу javascript!';
+// alert(some.length);
+
+// var some ='я учу javascript!';
+// alert(some.substr(2,14));
+// alert(some.substring(2,16));
+// alert(some.slice(2,16));
+
+// var some ='я учу javascript!';
+// alert(some.indexOf('учу'));
+
+// var str = 'lorem ipsum dolprasd';
+// var n = 10;
+// var result;
+// if (str.length > n) {
+//     result = str.substr(0, n) +  '...';
+// }  else {
+//     result = str;
+// }
+// alert(result);
+
+// var str = 'Я-учу-javascript!';
+// alert(str.replace(/-/g, '!'));
+
+// var str ='Я-учу-javascript!';
+// var arr = str.split(' ');
+// alert(arr);
+
+// var str ='Я-учу-javascript!';
+// var arr = str.split('');
+// alert(arr);
+
+// var date = '2025-12-31';
+// var arr = date.split('-');
+// alert(arr[2] + '.' + arr[1] + '.' + arr[0]);
+
+// var arr =  ['я', 'учу', 'javascript', '!'];
+// var str = arr.join('+');
+// alert(str);
+
+// var str ='я-учу-javascript!';
+// var n = str.slice(0,1).toUpperCase();
+// alert(n);
+
+// var str = 'я учу javascript!';
+// var arr = str.split(' ');
+// for (var i = 0; i < arr.length; i++) {
+// 	arr[i] = arr[i].slice(0, 1).toUpperCase() + arr[i].slice(1);
+// }
+// alert(arr);
+
+// var str = 'var_test_text';
+// var arr = str.split('_');
+// for (var i = 1; i < arr.length; i++) { 
+// 	arr[i] = arr[i].slice(0, 1).toUpperCase() + arr[i].slice(1);
+// }
+// var result = arr.join('');
+// alert(result);

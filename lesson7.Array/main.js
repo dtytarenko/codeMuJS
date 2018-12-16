070e0016c83739dbd61ae013a0c7a196d8edee4d
DmitriyTitarenko <dmutro.tutarenko@gmail.com>
2018-12-16 11:21:02 +0200

    work with array

---
+++ lesson7.Array/main.js
@@ -0,0 +1,14 @@
+// var a = [1, 2, 3];
+//var b = [4, 5, 6];
+
+//var c = a.concat(b);
+//alert(c);
+
+
+// var arr = [1, 2, 3];
+// alert(arr.reverse());
+
+var arr = [1, 2, 3];
+//arr.push(4, 5, 6);
+arr.unshift(4, 5, 6);
+alert(arr);

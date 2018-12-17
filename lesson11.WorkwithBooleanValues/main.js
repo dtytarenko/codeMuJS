c9a469bbf35d528efb95112c0082086ca3f7fccc
DmitriyTitarenko <dmutro.tutarenko@gmail.com>
2018-12-17 18:11:26 +0200

    end 11 and 12 lesson

---
+++ lesson11.WorkwithBooleanValues/main.js
@@ -0,0 +1,17 @@
+// function check (a , b){
+//     return a == b;
+// }
+
+// alert(check(2,3));
+
+// function check (a, b){
+//     return a + b > 10;
+// }
+
+// alert(check(5,5));
+
+// function check (a){
+//     return a < 0;
+// }
+
+// alert(check(2));
---
+++ lesson12.workWithLoop/main.js
@@ -0,0 +1,40 @@
+// let str = '';
+// for(let i = 2; i <= 9; i++) {
+//   for (let j = 2; j <= 9; j++) {
+//     str += (i * j) + ' ';
+//   }
+//   console.log(str);
+//   str = '';
+// }
+
+// str = '';
+// for (let i = 1; i <= 9; i++) {
+//     str += i;
+// }
+// console.log(str);
+
+// let str = '';
+// for (let i = 1; i <= 9; i++) {
+//     str += '-' + i;
+// }
+// console.log(str);
+
+
+// let str = '';
+// for (let i = 0; i <= 20; i++){
+//     str += 'x';
+//     document.write(str + '<br>');
+// }
+
+// for (var i = 1; i <= 9; i++){
+//     for (var j = 1; j <= i; j++){
+//         document.write(i);
+//     }
+//     document.write('<br>');
+// }
+
+// let str = '';
+// for (let i = 0; i <= 10; i++){
+//     str += 'x';
+//     document.write(str + '<br>');
+// }

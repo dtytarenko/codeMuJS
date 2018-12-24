659bb00fa9acd10c2e437c14f0b54f95bf540ba9
DmitriyTitarenko <dmutro.tutarenko@gmail.com>
2018-12-24 19:19:23 +0200

    end 15 lesson

---
+++ lesson15.CustomFunction/main.js
@@ -0,0 +1,48 @@
+// var str ='asda dfs dsf';
+// var newArr = [];
+
+// var arr = str.split(' ');
+// for (var i = 0; i < arr.length; i++) {
+//     newArr.push(ucfirst(arr[i]))
+// }
+
+// var newStr = newArr.join(' ');
+
+// function ucfirst(str) {
+//     return str[0].toUpperCase() + str.substr(i);
+// }
+
+// let str = 'var_text_hello';
+// let newStr = str.split('_');
+// for (let i = 1; i < newStr.length; i ++) {
+//     newStr[i] = ucfirst(arr[i]);
+// }
+
+// str = newStr.join('');
+
+
+// function inArray(str, arr) {
+// 	for (var i = 0; i < arr.length; i++) {
+// 		if (arr[i] === str) {
+// 			return true;
+// 		}
+// 	}
+// 	return false;
+// }
+
+// var str = 'яблоко';
+// var arr = ['банан', 'яблоко', 'персик'];
+// console.log(inArray(str, arr));
+
+
+// str = '123456';
+// arr = str.split('');
+// for(let i = 0; i < arr.length; i++) {
+//   if(i % 2 !== 0) {
+//     let b = arr[i - 1];
+//     arr[i - 1] = arr[i];
+//     arr[i] = b;
+//   }
+// }
+// str = arr.join('')
+// console.log(str)

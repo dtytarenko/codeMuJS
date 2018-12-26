6013a0f5eeb3a7ef071988c1334c3a983c8f5884
DmitriyTitarenko <dmutro.tutarenko@gmail.com>
2018-12-26 15:24:41 +0200

    solve first lesson of BasicDOM

---
+++ BasicDOM/lesson1.BasicDOM/main.js
@@ -0,0 +1,115 @@
+// function buttonClick() {
+//     let input = document.getElementById('input');
+//     input.value= '???';
+// }
+
+// function buttonClick(){
+//     let input = document.getElementById('input');
+//     alert(input.value);
+// }
+
+// function buttonClick() {
+//     let input = document.getElementById('input');
+//     let number = Number(input.value);
+//     let square = number * number;
+//     alert(square);  
+// }
+
+// function buttonClick() {
+//     let firstInput = document.getElementById('input1');
+//     let secondInput = document.getElementById('input2');
+//     let firstValue = firstInput.value;
+//     let secondValue = secondInput.value;
+//     firstInput.value = secondValue;
+//     secondInput.value = firstValue;
+// }
+
+
+// function buttonClick(elem) {
+//     elem.value = 'new text';
+// }
+
+// function buttonClick(){
+//     // let input = document.getElementById('input');
+//     // input.style.color = 'blue';
+//     document.getElementById('input').style.color = 'blue';
+// }
+
+// let input = document.getElementById('input');
+
+// function button1Click() {
+//     input.disabled = true;
+// }
+
+// function button2Click() {
+//     input.disabled = false;
+// }
+
+// function buttonClick (){
+//     let input = document.getElementById('input')
+//     alert(input.value);
+// }
+
+
+// function buttonClick() {
+//     let input = document.getElementById('input');
+//     input.value = 'change text';
+// }
+
+// function buttonClick() {
+//     let img = document.getElementById('img');
+//     img.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Macaca_sinica_-_01.jpg/220px-Macaca_sinica_-_01.jpg";
+// }
+
+
+// function go(elem){
+//     alert(elem.value);
+// }
+
+// function go(elem){
+//     elem.value = 'change text';
+// }
+
+// function go(elem){
+//     elem.value = "first change";
+// }
+
+// function stop(elem){
+//     elem.value = "second change";
+// }
+
+// function go(elem){
+//     elem.value = "bye";
+// }
+
+// function go(elem){
+//     elem.disabled = true;
+//     elem.value = 'fuck u';
+// } 
+
+// function go(elem) {
+//     elem.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Macaca_sinica_-_01.jpg/220px-Macaca_sinica_-_01.jpg";
+// }
+
+// function stop(elem){
+//     elem.src ="https://thumbs-prod.si-cdn.com/PqfMd1XgAM9LznllA8VDjcCJGUI=/800x600/filters:no_upscale()/https://public-media.si-cdn.com/filer/38/ee/38ee7aaa-abbb-4c60-b959-4b1c5c8c6989/axgpay.jpg"
+// }
+
+// function go(){
+//     document.getElementById('input').style.color = 'red';
+// }
+
+// function go(){
+//     document.getElementById('input').style.display = 'none';
+// }
+
+// function stop(){
+//     document.getElementById('input').style.display = 'inline-block';
+// }
+
+// function go(){
+//    let input = document.getElementById('input');
+//     input.style.height = '300px';
+//     input.style.color = 'red';
+//     input.style.borderRadius = '50px';
+// }

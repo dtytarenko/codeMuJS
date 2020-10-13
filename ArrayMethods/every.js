f10a285ef70e6c1688ba9f6fd7f554f08fb27f57
dtytarenko <dmutro.tutarenko@gmail.com>
2020-10-13 17:20:37 +0300

    finished training every and some method js

---
+++ ArrayMethods/every.js
@@ -0,0 +1,7 @@
+// let arr = [2, 4, 6, 8, ];
+// let result = arr.every(elem => elem > 0);
+// console.log(result);
+
+// let arr = [2, 4, 6, 8, ];
+// let result = arr.every((elem, index) => elem * index < 0);
+// console.log(result)
---
+++ ArrayMethods/some.js
@@ -0,0 +1,7 @@
+// let arr = [2, 4, 6, 8];
+// let result = arr.some(elem => elem > 0);
+// console.log(result);
+
+// let arr = [2, 4, 6, 8];
+// let result = arr.some((elem, index) => elem * index < 30);
+// console.log(result);

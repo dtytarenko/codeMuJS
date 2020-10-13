6339dbaedf4dfeb09f6705fb081649d9f9b92aec
dtytarenko <dmutro.tutarenko@gmail.com>
2020-10-13 23:28:00 +0300

    finished training recursion

---
+++ Recursion/recursion.js
@@ -0,0 +1,15 @@
+let result = [];
+
+const fibonacci = (prevPrevNum, prevNum) => {
+    let sum = prevPrevNum + prevNum;
+    result.push(sum);
+
+    if (result.length < 10){
+        fibonacci(prevNum, sum);
+    }
+    
+    return result;
+}
+fibonacci(1, 2);
+console.log(result);
+

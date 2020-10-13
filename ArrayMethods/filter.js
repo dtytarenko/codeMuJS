eba85f59f6c164f26cb6f5fba404e4c866848964
dtytarenko <dmutro.tutarenko@gmail.com>
2020-10-13 17:10:42 +0300

    finished training filter method

---
+++ ArrayMethods/filter.js
@@ -0,0 +1,27 @@
+// let arr = [1, 2, -4, 6, 7, -3];
+// let results = arr.filter((elem) => elem > 0);
+// console.log(results); 
+
+// let arr = [1, 2, -4, 6, 7, -3];
+// let results = arr.filter((elem) => elem < 0);
+// console.log(results); 
+
+// let arr = [1, 11, 2, -4, 6, 7, -3, 12, 14, 16];
+// let results = arr.filter((elem) => elem > 0 && elem < 10);
+// console.log(results); 
+
+// let arr = ['apple', 'lemon', 'avocado', 'pineapple', 'tea'];
+// let results = arr.filter((elem) => elem.length > 5);
+// console.log(results);
+
+// let arr = [1, 11, 2, -4, 6, 7, -3, 12, 14, 16];
+// let results = arr.filter((elem, index) => elem * index < 30);
+// console.log(results);
+
+// let arr = [1, 2, [3, 4], 5, [6, 7]];
+// let results = arr.filter((elem) => !Array.isArray(elem));
+// console.log(results);
+
+// let arr = [1, 2, -4, 6, 7, -3];
+// let results = arr.filter(elem => elem < 0).length;
+// console.log(results);
